e25c925395384a2850a79572599d4cdd4c58261b
wyy <18610934392@163.com>
2017-12-06 21:52:08 +0800

    e3-manager sec

--- e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -1,11 +1,18 @@
 package cn.e3.manager.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 import cn.e3.manager.service.ItemService;
 import cn.e3.mapper.TbItemMapper;
 import cn.e3.pojo.TbItem;
+import cn.e3.pojo.TbItemExample;
+import cn.e3.utils.DatagridPagebean;
 @Service
 public class ItemServiceImpl implements ItemService {
 	
@@ -20,6 +27,20 @@ public class ItemServiceImpl implements ItemService {
 		// 根据主键查询商品数据
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+
+	@Override
+	public DatagridPagebean findItemListByPage(Integer page,Integer rows) {
+		TbItemExample example=new TbItemExample();
+		PageHelper.startPage(page, rows);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		DatagridPagebean pageBean=new DatagridPagebean();
+		PageInfo<TbItem> pageinfo=new PageInfo<>(list);
+		pageBean.setTotal(pageinfo.getTotal());
+		pageBean.setRows(list);
+		
+		return pageBean;
 	}
 
 }
